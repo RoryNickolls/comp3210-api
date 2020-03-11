61cee05152186240689d07f78c9cba877f87c5eb
RoryNickolls <rorynickolls@gmail.com>
2020-03-11 14:24:27 +0000

    Randomly generate a password when a lock is registered

--- main.go
+++ main.go
@@ -1,11 +1,14 @@
 package main
 
 import (
+	"crypto/rand"
 	"encoding/json"
 	"fmt"
 	"log"
+	"math/big"
 	"net/http"
 	"strconv"
+	"strings"
 
 	"github.com/gorilla/mux"
 )
@@ -22,6 +25,7 @@ func root(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusOK)
 }
 
+// Register a lock to this account
 func register(w http.ResponseWriter, r *http.Request) {
 	var Data struct {
 		Name   string
@@ -34,11 +38,25 @@ func register(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	l := Lock{Data.Name, Data.Serial, "open sesame"}
+	// Create a cryptographically random password
+	chars := []rune("ABCDEFGHIJKLMNOPQRSTUVWXYZ" +
+		"abcdefghijklmnopqrstuvwxyz" +
+		"0123456789")
+	charsLen := big.NewInt(int64(len(chars)))
+	length := 32
+	var b strings.Builder
+	for i := 0; i < length; i++ {
+		pos, _ := rand.Int(rand.Reader, charsLen)
+		b.WriteRune(chars[pos.Int64()])
+	}
+
+	// Create a lock with the given data and generated password
+	l := Lock{Data.Name, Data.Serial, b.String()}
 	registeredLocks = append(registeredLocks, l)
 	json.NewEncoder(w).Encode(l)
 }
 
+// Request access to a lock
 func access(w http.ResponseWriter, r *http.Request) {
 	params := mux.Vars(r)
 
@@ -47,6 +65,7 @@ func access(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(l)
 }
 
+// Get the list of registered locks
 func locks(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(registeredLocks)
@@ -60,5 +79,6 @@ func main() {
 	r.HandleFunc("/locks", locks).Methods(http.MethodGet)
 	r.HandleFunc("/locks/{id}/access", access).Methods(http.MethodGet)
 	r.HandleFunc("/locks", register).Methods(http.MethodPost)
+	//log.Fatal(http.ListenAndServeTLS(":8080", "cert.crt", "key.key", r))
 	log.Fatal(http.ListenAndServe(":8080", r))
 }
